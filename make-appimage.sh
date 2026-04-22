c4c9e91f26e262c2cc3c425955889fb7f7d9a2ea
Link4Electronics <link4electronics@gmail.com>
2026-04-21 23:28:59 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,11 +8,11 @@ export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export DESKTOP=https://github.com/tomluchowski/OpenDungeonsPlus/raw/refs/heads/shaders-improvement/io.github.tomluchowski.OpenDungeonsPlus.desktop
 export DEPLOY_OPENGL=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun ./AppDir/bin/opendungeon-plus
 
 # Additional changes can be done in between here
 
