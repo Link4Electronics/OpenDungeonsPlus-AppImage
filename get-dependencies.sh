015c7281b2b91bb8385b2514d7b485ccf51f97bd
Link4Electronics <link4electronics@gmail.com>
2026-04-21 22:40:24 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -10,7 +10,10 @@ pacman -Syu --noconfirm \
     boost      \
     boost-libs \
     cmake      \
+    ogre       \
     pugixml    \
+    pybind11   \
+    python     \
     sfml
 
 echo "Installing debloated packages..."
@@ -18,7 +21,7 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+make-aur-package cegui
 
 # If the application needs to be manually built that has to be done down here
 
